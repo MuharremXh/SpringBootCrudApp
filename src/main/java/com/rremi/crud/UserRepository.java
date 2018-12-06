75f0d84caf5974f5e67d2e0597d2bc9a8e7f65bd
Rremi <xhixha.rremi@yahoo.com>
2018-12-06 01:03:11 -1100

    V1

---
+++ src/main/java/com/rremi/crud/UserRepository.java
@@ -0,0 +1,8 @@
+package com.rremi.crud;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+} //will be automatically implemented
